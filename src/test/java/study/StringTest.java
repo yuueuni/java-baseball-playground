6501c576c30e6a362c967eed12476772c13085c1
yuueuni <yuueuni@gmail.com>
2022-07-22 00:16:03 +0900

    add requirement 1 -  3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,19 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void replace1() {
+        String[] require = "1,2".split(",");
+        String[] require1 = "1".split(",");
+
+        assertThat(require).contains("1", "2");
+        assertThat(require1).containsExactly("1");
+    }
+
+    @Test
+    void replace2() {
+        String require2 = "(1,2)".replace("(", "").replace(")", "");
+        assertThat(require2).isEqualTo("1,2");
+    }
 }
